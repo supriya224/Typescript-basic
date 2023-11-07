c767a34c9bd7d5e3246fba7421dea507d4737329
supriya224 <52038704+supriya224@users.noreply.github.com>
2023-11-07 23:44:03 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/components/core/counter/Counter.tsx
+++ src/components/core/counter/Counter.tsx
@@ -13,6 +13,7 @@ const Counter: React.FC = () => {
   }
   const removeValue = () => {
     setState({ count: state.count - 1 })
+  
   }
   const reset = () => {
     setState({ count: 0 })
@@ -20,24 +21,26 @@ const Counter: React.FC = () => {
  
 
   return (
-    <div className="container w-[50vw] h-[50vh] sm:w-1/2 md:flex sm:mt-0 flex flex-wrap  mx-[25rem] my-0 py-9 gap-4 fixed bg-indigo-500 text-white rounded-md shadow-xl shadow-indigo-700 text-center items-center justify-center">
-      <h1 className="text-2xl font-bold ">Assignment of Analystt.ai</h1>
-      <div className="mx-auto my-9 justify-between gap-9">
-        <h1 className="w-28 h-16 mx-72 my-20 text-center justify-center p-4 text-2xl rounded-md bg-indigo-800">{state.count}</h1>
-        <div className="justify-between w-full h-12 flex container text-lg ">
-          <label htmlFor="" className="mx-20">Add
-            <button onClick={addValue} className=" w-20 h-10 rounded-lg shadow-md shadow-white bg-black hover:bg-gray-800 hover:scale-110 duration-200 ease-in"  >+</button>
-          </label>
-          <label htmlFor="" className="mx-20">Reset
-            <button onClick={reset} className=" w-24 h-10 rounded-lg shadow-md shadow-white bg-black hover:bg-gray-800 hover:scale-110 duration-200 ease-in" >Reset</button>
-          </label>
-          <label htmlFor="" className="mx-20">Remove
-            <button onClick={removeValue} className=" w-20 h-10 rounded-lg shadow-md shadow-white  bg-black hover:bg-gray-800 hover:scale-110 duration-200 ease-in" >-</button>
-          </label>
+    <div className="container w-96 sm:w-[50vw] h-[60vh] md:flex sm:mt-0 flex flex-wrap  mx-[25rem] my-0 py-5 gap-4 bg-indigo-500 text-white rounded-md shadow-xl shadow-indigo-700 text-center items-center justify-center">
+    <h1 className="text-2xl max-w font-bold ">Assignment of Analystt.ai</h1>
+    <div className="mx-auto my-9 justify-between gap-9 sm:max-md:">
+      <h1 className="w-28 h-16 mx-72 my-20 text-center justify-center p-4 text-2xl rounded-md bg-indigo-800">{state.count}</h1>
+      <div className="justify-between w-full h-12 flex container text-lg sm:flex xs:w-28 ">
+        <label htmlFor="" className="mx-20">Add
+          <button onClick={addValue} className=" w-20 h-10 rounded-lg shadow-md shadow-white bg-black hover:bg-gray-800 hover:scale-110 duration-200 ease-in"  >+</button>
+        </label>
+        <label htmlFor="" className="mx-20">Reset
+          <button onClick={reset} className=" w-24 h-10 rounded-lg shadow-md shadow-white bg-black hover:bg-gray-800 hover:scale-110 duration-200 ease-in" >Reset</button>
+        </label>
+        <label htmlFor="" className="mx-20">Remove
+          <button onClick={removeValue} className=" w-20 h-10 rounded-lg shadow-md shadow-white  bg-black hover:bg-gray-800 hover:scale-110 duration-200 ease-in" >-</button>
+        </label>
 
-        </div>
       </div>
     </div>
+  </div>
+
+   
   )
 }
 
